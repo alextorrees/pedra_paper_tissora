7a87975663134394de9c0c53c957b096ac180487
Alex Torres <144930355+alextorrees@users.noreply.github.com>
2023-11-13 19:30:47 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let decision = 0
+input.onGesture(Gesture.Shake, function () {
+    decision = randint(0, 2)
+    if (decision == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (decision == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
